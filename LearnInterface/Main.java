b46278000e2bafb7f029a7dc55e9e1ff1564118a
eustrain1246 <55869857+eustrain1246@users.noreply.github.com>
2020-07-24 17:19:47 +0800

    Delete Main.java

--- LearnInterface/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main{
-    public static void main(String[] args) {
-        myInterface myInterface = new myRealize1();
-        myInterface.abstractMethod1();
-        myInterface.defaultMethod1();
-    }
-}
