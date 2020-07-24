f6302514e40946b6ec8584b6f8677d7a89813b04
eustrain1246 <55869857+eustrain1246@users.noreply.github.com>
2020-07-24 16:26:41 +0800

    Create Main.java

---
+++ LearnInterface/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        myInterface myInterface = new myRealize1();
+        myInterface.abstractMethod1();
+        myInterface.defaultMethod1();
+    }
+}
