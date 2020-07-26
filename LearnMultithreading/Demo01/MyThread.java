79cbd873c2214df72e047d9de2909437f0192b53
eustrain1246 <55869857+eustrain1246@users.noreply.github.com>
2020-07-26 13:04:44 +0800

    Update MyThread.java

--- LearnMultithreading/Demo01/MyThread.java
+++ LearnMultithreading/Demo01/MyThread.java
@@ -7,5 +7,15 @@ public class MyThread extends Thread{
         Thread thread = Thread.currentThread();     //获得运行该段代码的线程，因为run方法不一定在特定的新开辟的线程中运行
                                                     //例如可以直接通过 对象.run(); 直接调用函数
         System.out.println("CurrentThread Id: " + thread.getId() + " Name: " + thread.getName());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("run over");
+    }
+
+    public MyThread(String name) {
+        super("MyThread:" + name);
     }
 }
