0a1a2b8087ed0e269cc4fbf81f2603b258cbd0ad
eustrain1246 <55869857+eustrain1246@users.noreply.github.com>
2020-07-28 14:29:31 +0800

    Update Main.java

--- LearnStream/Demo01/Main.java
+++ LearnStream/Demo01/Main.java
@@ -9,24 +9,24 @@ class Main{
     public static void func1(){
         try {
             /*
-              创建一个FileOutput流，参数是String或者File对象
+              创建一个FileOutput流，参数是String或者File对象,如果找不到路径会抛出一个异常
               在对象创建的时候，
               如果没有文件就创建文件
               第二个参数为false或者没有写第二个参数时
               如果有文件，就把之前的文件删掉，重新创建一个
               而第二个参数为true时，不会删除原文件，后面的写入操作会直接写在原数据尾部
              */
-            FileOutputStream fileOutputStream = new FileOutputStream(new File("Note.txt"),true);
+            FileOutputStream fos = new FileOutputStream(new File("Note.txt"),true);
             //向文件中写入一字节的数据，就目前的知识，如果小于0，不能确定写入了什么,大于255行为也不清楚，因为超出了一字节
-            fileOutputStream.write(65);// A
+            fos.write(65);// A
             //向指定的文件写入一段Byte[]数据
-            fileOutputStream.write("Dates 数据\n".getBytes());//换行符要注意操作系统
+            fos.write("Dates 数据\n".getBytes());//换行符要注意操作系统
             //向指定的文件写入一段Byte[]数据，偏移量最小为0（不偏移），长度
-            fileOutputStream.write("Hello World!".getBytes(),6,5);
+            fos.write("Hello World!".getBytes(),6,5);
             //使用完后记得调用close()方法关闭此文件输入流并释放与此流有关的所有系统资源。
-            fileOutputStream.close();
+            fos.close();
             //close()之后不能再次写入数据
-            //fileOutputStream.write("Dates 数据".getBytes());
+            //fos.write("Dates 数据".getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
