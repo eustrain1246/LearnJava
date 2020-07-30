13bcee077d46273048afc4fc0caf85eaf8e1c9f1
eustrain1246 <55869857+eustrain1246@users.noreply.github.com>
2020-07-30 18:51:28 +0800

    Create MyClass.java

---
+++ LearnMethodReference/Demo03/MyClass.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    String value;
+
+    public MyClass(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
